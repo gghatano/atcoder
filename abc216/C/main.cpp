4915676e8df1e781c60d57fd1f0b4395d1cb0649
gghatano <adad.0405@gmail.com>
2021-09-05 02:31:13 +0000

    abc216 C

--- abc216/C/main.cpp
+++ abc216/C/main.cpp
@@ -14,4 +14,19 @@ ll INF = numeric_limits<ll>::max() / 2;
 int main(){
   // set precision (10 digit)
   cout << setprecision(10);
+  ll N; cin >> N;
+
+  string ans = "";
+  while(N > 0){
+    if(N%2 == 0){
+      ans += "B";
+      N /= 2;
+    } else {
+      ans += "A";
+      N--;
+    }
+  }
+
+  reverse(ans.begin(), ans.end());
+  cout << ans << endl;
 }
