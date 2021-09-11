bb2f4cfbe3e399d710cd365839d6b29578169a24
gghatano <adad.0405@gmail.com>
2021-09-11 23:36:52 +0000

    Dabc217/D

--- abc217/D/main.cpp
+++ abc217/D/main.cpp
@@ -13,5 +13,28 @@ ll INF = numeric_limits<ll>::max() / 2;
 
 int main(){
   // set precision (10 digit)
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
   cout << setprecision(10);
+
+  set<ll> st;
+
+  ll L; int Q; cin >> L >> Q;
+
+  st.insert(0);
+  st.insert(L);
+
+  while(Q--){
+    int c; ll x; cin >> c >> x;
+
+    if(c == 1){
+      st.insert(x);
+    } else {
+      auto index = st.lower_bound(x);
+      auto before = prev(index);
+      cout << *index - *before << endl;
+
+    }
+
+  }
 }
--- abc217/E/main.cpp
+++ abc217/E/main.cpp
@@ -11,7 +11,36 @@ using mint = static_modint<1000000007>;
 // ll int
 ll INF = numeric_limits<ll>::max() / 2;
 
+template<class T>
+using MinHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
 int main(){
   // set precision (10 digit)
   cout << setprecision(10);
+  int Q; cin >> Q;
+
+  queue<ll> q;
+  MinHeap<ll> pq;
+
+  while(Q--){
+    int t; cin >> t;
+    if(t == 1){
+      ll x; cin >> x;
+      q.push(x);
+    } else if(t == 2){
+      if(pq.size() > 0){
+        cout << pq.top() << endl;
+        pq.pop();
+      } else {
+        cout << q.front() << endl;
+        q.pop();
+      }
+    } else {
+      while(q.size() > 0){
+        ll tmp = q.front();
+        q.pop();
+        pq.push(tmp);
+      }
+    }
+  }
 }
