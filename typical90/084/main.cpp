f1664effe732edfbda58bf0051571a8f77a20b2c
gghatano <adad.0405@gmail.com>
2021-09-12 06:50:00 +0000

    084

--- typical90/084/main.cpp
+++ typical90/084/main.cpp
@@ -14,4 +14,35 @@ ll INF = numeric_limits<ll>::max() / 2;
 int main(){
   // set precision (10 digit)
   cout << setprecision(10);
+  ll N; string S; cin >> N; cin >> S;
+  vector<ll> v;
+
+  char before;
+  ll num = 0;
+  for(int i = 0; i < S.size(); i++){
+    if(i == 0){
+      before = S[i];
+      num = 1;
+    } else if(S[i] == before){
+      num++;
+    } else {
+      v.emplace_back(num);
+      before = S[i];
+      num = 1;
+    }
+
+  }
+  if(num > 0){
+    v.emplace_back(num);
+  }
+
+  ll ans = N * (N-1);
+  ans /= 2;
+
+  for(auto vv: v){
+    ll tmp = vv * (vv-1);
+    tmp /= 2;
+    ans -= tmp;
+  }
+  cout << ans << endl;
 }
