a17fdd2735e038feb9863cd10ab8b21d80e1a9c7
gghatano <adad.0405@gmail.com>
2021-09-13 09:29:33 +0000

    085

--- typical90/014/main.cpp
+++ typical90/014/main.cpp
@@ -14,4 +14,17 @@ ll INF = numeric_limits<ll>::max() / 2;
 int main(){
   // set precision (10 digit)
   cout << setprecision(10);
+  int N; cin >> N;
+  vector<ll> A(N), B(N);
+  for(int i = 0; i < N; i++) cin >> A[i];
+  for(int i = 0; i < N; i++) cin >> B[i];
+
+  sort(A.begin(), A.end());
+  sort(B.begin(), B.end());
+
+  ll ans = 0;
+  for(int i = 0; i < N; i++){
+    ans += abs(A[i] - B[i]);
+  }
+  cout << ans << endl;
 }
--- typical90/085/main.cpp
+++ typical90/085/main.cpp
@@ -11,7 +11,40 @@ using mint = static_modint<1000000007>;
 // ll int
 ll INF = numeric_limits<ll>::max() / 2;
 
+// 約数列挙 O(√n)
+vector<ll> divisor(ll n){
+  vector<ll> res;
+  for(ll i = 1; i * i <= n; i++){
+    if(n % i == 0){
+      res.push_back(i);
+      if(i != n / i) res.push_back(n / i);
+    }
+  }
+  return res;
+}
+
 int main(){
   // set precision (10 digit)
   cout << setprecision(10);
+  ll k; cin >> k;
+  auto v = divisor(k);
+
+  sort(v.begin(), v.end());
+
+  ll ans = 0;
+  for(int i = 0; i < v.size() && v[i] * v[i] <= k; i++){
+    for(int j = 0; j <= i; j++){
+      ll tmp = v[i] * v[j];
+      if(k % tmp != 0) continue;
+
+      ll c = k / tmp;
+      if(c >= v[i]){
+        ans++;
+      } else {
+        continue;
+      }
+    }
+  }
+  cout << ans << endl;
 }
+
