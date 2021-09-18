046a8c98b4c3142fa20f5e7a87bdade446ce5077
gghatano <adad.0405@gmail.com>
2021-09-18 23:20:57 +0000

    029

--- typical90/029/main.cpp
+++ typical90/029/main.cpp
@@ -9,9 +9,32 @@ typedef pair<int,int> pii;
 // mint
 using mint = static_modint<1000000007>;
 // ll int
-ll INF = numeric_limits<ll>::max() / 2;
-
+// ll INF = numeric_limits<ll>::max() / 2;
+using S = long long;
+using F = long long;
+ 
+const S INF = 8e18;
+const F ID = 8e18;
+ 
+S op(S a, S b){ return std::max(a, b); }
+S e(){ return -INF; }
+S mapping(F f, S x){ return (f == ID ? x : f); }
+F composition(F f, F g){ return (f == ID ? g : f); }
+F id(){ return ID; }
+ 
 int main(){
-  // set precision (10 digit)
-  cout << setprecision(10);
+  int W,N; cin >> W >> N;
+  vector<S> v(W+1);
+  lazy_segtree<S, op, e, F, mapping, composition, id> seg(v);
+
+  for(int i = 0; i < N; i++){
+    int x,y; cin >> x >> y;
+    auto ans = seg.prod(x, y+1);
+    cout << ans+1 << endl;
+
+    seg.apply(x, y+1, ans+1);
+  }
+
+
 }
+
--- typical90/081/main.cpp
+++ typical90/081/main.cpp
@@ -14,4 +14,42 @@ ll INF = numeric_limits<ll>::max() / 2;
 int main(){
   // set precision (10 digit)
   cout << setprecision(10);
+  int N,K; cin >> N >> K;
+  int M = 5000;
+  vector<vector<int>> A(M+1, vector<int>(M+1, 0));
+
+  for(int i = 0; i < N; i++){
+    int x,y; cin >> x >> y;
+
+    int xmin = max(0, x-K);
+    int xmax = min(M, x+K+1);
+    int ymin = max(0, y-K);
+    int ymax = min(M, y+K+1);
+
+    A[xmax][ymax]++;
+    A[xmax][ymin]--;
+    A[xmin][ymax]--;
+    A[xmin][ymin]++;
+  }
+
+  for(int i = 0; i <= M; i++){
+    for(int j = 1; j <= M; j++){
+      A[i][j] += A[i][j-1];
+    }
+  }
+  for(int j = 0; j <= M; j++){
+    for(int i = 1; i <= M; i++){
+      A[i][j] += A[i-1][j];
+    }
+  }
+
+  int ans = 0;
+  for(int i = 0; i <= M; i++){
+    for(int j = 0; j <= M; j++){
+      ans = max(ans, A[i][j]);
+    }
+  }
+  cout << ans << endl;
+
+
 }
